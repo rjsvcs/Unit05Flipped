6c60d9d36e609a2cd1e488d8b6c8dfc01629839f
Bobby St. Jacques <rjs@se.rit.edu>
2025-02-16 14:58:41 -0500

    for each example

---
+++ src/main/java/unit05/ForExample.java
@@ -0,0 +1,18 @@
+package unit05;
+
+public class ForExample {
+    public static void main(String[] args) {
+        int[] array = { 0, 9, 1, 8, 2, 7, 3, 6, 4, 5 };
+
+        for (int i=0; i<array.length; i++) {
+            int element = array[i];
+            System.out.print(element + " ");
+        }
+        System.out.println();
+
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+}
